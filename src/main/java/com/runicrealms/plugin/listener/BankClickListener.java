88f13da404349bc863f3b82efa3a6d83b1fc965d
Skyfallin <zextertech777@yahoo.com>
2020-05-11 05:11:24 -0700

    fixed small bug w/ inv listener

--- src/main/java/com/runicrealms/plugin/listener/BankClickListener.java
+++ src/main/java/com/runicrealms/plugin/listener/BankClickListener.java
@@ -15,37 +15,44 @@ public class BankClickListener implements Listener {
 
     @EventHandler
     public void clickEvent(InventoryClickEvent e) {
-        if (!(e.getWhoClicked() instanceof Player)) return;
-        Player pl = (Player) e.getWhoClicked();
-        // disable interactions on first 9 slots
-        if (RunicBank.getBankManager().getStorages() == null) {
-            Bukkit.getLogger().info("bank manager storages in null");
-            pl.closeInventory();
-            return;
-        }
-        if (RunicBank.getBankManager().getStorages().get(pl.getUniqueId()) == null) {
-            Bukkit.getLogger().info("bank storage for player");
-            pl.closeInventory();
-            return;
-        }
-        String bankTitle = RunicBank.getBankManager().getStorages().get(pl.getUniqueId()).getBankTitle();
-        if (e.getView().getTitle().equalsIgnoreCase(bankTitle)) {
-            if (e.getClickedInventory() != null
-                    && !(e.getClickedInventory() instanceof PlayerInventory)
-                    && e.getView().getTitle().equals(bankTitle) && e.getSlot() < 9) {
-                pl.playSound(pl.getLocation(), Sound.UI_BUTTON_CLICK, 0.5f, 1.0f);
-                e.setCancelled(true);
-                BankStorage storage = RunicBank.getBankManager().getStorages().get(pl.getUniqueId());
-                switch (e.getSlot()) {
-                    case 6:
-                        storage.addPage(pl.getUniqueId(), e.getCurrentItem().getType());
-                        break;
-                    case 7:
-                        storage.prevPage(pl.getUniqueId());
-                        break;
-                    case 8:
-                        storage.nextPage(pl.getUniqueId());
-                        break;
+
+        // verify that we're looking at a bank inv
+        if (e.getView().getTitle().equals
+                (RunicBank.getBankManager().getStorages().get(e.getWhoClicked().getUniqueId()).getBankTitle())) {
+
+            if (!(e.getWhoClicked() instanceof Player)) return;
+            Player pl = (Player) e.getWhoClicked();
+
+            // disable interactions on first 9 slots
+            if (RunicBank.getBankManager().getStorages() == null) {
+                Bukkit.getLogger().info("bank manager storages in null");
+                pl.closeInventory();
+                return;
+            }
+            if (RunicBank.getBankManager().getStorages().get(pl.getUniqueId()) == null) {
+                Bukkit.getLogger().info("Error: bank storage for " + pl.getName() + " is null!");
+                pl.closeInventory();
+                return;
+            }
+            String bankTitle = RunicBank.getBankManager().getStorages().get(pl.getUniqueId()).getBankTitle();
+            if (e.getView().getTitle().equalsIgnoreCase(bankTitle)) {
+                if (e.getClickedInventory() != null
+                        && !(e.getClickedInventory() instanceof PlayerInventory)
+                        && e.getView().getTitle().equals(bankTitle) && e.getSlot() < 9) {
+                    pl.playSound(pl.getLocation(), Sound.UI_BUTTON_CLICK, 0.5f, 1.0f);
+                    e.setCancelled(true);
+                    BankStorage storage = RunicBank.getBankManager().getStorages().get(pl.getUniqueId());
+                    switch (e.getSlot()) {
+                        case 6:
+                            storage.addPage(pl.getUniqueId(), e.getCurrentItem().getType());
+                            break;
+                        case 7:
+                            storage.prevPage(pl.getUniqueId());
+                            break;
+                        case 8:
+                            storage.nextPage(pl.getUniqueId());
+                            break;
+                    }
                 }
             }
         }
