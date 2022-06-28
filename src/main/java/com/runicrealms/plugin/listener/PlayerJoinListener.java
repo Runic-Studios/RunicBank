0992bd1ecbf9b9a30c32d047ad32b8f43990c73c
skyfallin <dimetrius.hightower@gmail.com>
2022-06-28 09:55:39 -0700

    updated to use character select event

--- src/main/java/com/runicrealms/plugin/listener/PlayerJoinListener.java
+++ src/main/java/com/runicrealms/plugin/listener/PlayerJoinListener.java
@@ -1,7 +1,7 @@
 package com.runicrealms.plugin.listener;
 
 import com.runicrealms.plugin.RunicCore;
-import com.runicrealms.plugin.character.api.CharacterLoadEvent;
+import com.runicrealms.plugin.character.api.CharacterSelectEvent;
 import com.runicrealms.plugin.util.DataUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
@@ -10,8 +10,8 @@ import org.bukkit.event.Listener;
 public class PlayerJoinListener implements Listener {
 
     @EventHandler
-    public void onCharacterLoad(CharacterLoadEvent e) {
-        Bukkit.getScheduler().scheduleAsyncDelayedTask(RunicCore.getInstance(),
+    public void onCharacterLoad(CharacterSelectEvent e) {
+        Bukkit.getScheduler().runTaskLaterAsynchronously(RunicCore.getInstance(),
                 () -> DataUtil.createBankOrLoad(e.getPlayer().getUniqueId()), 1L);
     }
 }
