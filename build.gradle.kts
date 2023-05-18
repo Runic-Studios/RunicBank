86dda5733981c2e9e30549f7c4ab8ad404e75e64
Excel619 <excel8392@gmail.com>
2023-05-18 16:26:56 -0600

    Added build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+group = "com.runicrealms.plugin"
+version = "1.0-SNAPSHOT"
+
+dependencies {
+    compileOnly(commonLibs.paper)
+    compileOnly(commonLibs.spigot)
+    compileOnly(project(":Projects:Core"))
+    compileOnly(project(":Projects:Items"))
+    compileOnly(project(":Projects:Npcs"))
+    compileOnly(project(":Projects:Restart"))
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.runicrealms.plugin"
+            artifactId = "bank"
+            version = "1.0-SNAPSHOT"
+            from(components["java"])
+        }
+    }
+}
+
+tasks.register("wrapper")
+tasks.register("prepareKotlinBuildScriptModel")
